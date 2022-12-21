c3b8de12b563dc0188f273d9526851f8faeb2379
¨Yoss1800¨ <contacto2000@gmail.com>
2022-12-21 14:37:55 -0300

    nextauth JWT enable, copy of old + google ok. sgnin compinent option - not yet functional

--- src/pages/api/auth/[...nextauth] sin JWT.ts
+++ src/pages/api/auth/[...nextauth] sin JWT.ts
@@ -8,7 +8,7 @@ import { prisma } from "../../../server/db/client";
 //import EmailProvider from 'next-auth/providers/email';
 //import { PrismaClient } from '@prisma/client';
 //import GitHubProvider from "next-auth/providers/github";
-//import CredentialsProvider from 'next-auth/providers/credentials';
+// import CredentialsProvider from 'next-auth/providers/credentials';
 
 //const prisma = new PrismaClient();
 
@@ -66,13 +66,12 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.JWT_SECRET,
 
   session: {
-    strategy: "jwt",
     // Use JSON Web Tokens for session instead of database sessions.
     // This option can be used with or without a database for users/accounts.
     // Note: `jwt` is automatically set to `true` if no database is specified.
     //jwt: true,
     // Seconds - How long until an idle session expires and is no longer valid.
-    maxAge: 30 * 24 * 60 * 60, // 30 days
+    // maxAge: 30 * 24 * 60 * 60, // 30 days
     // Seconds - Throttle how frequently to write to database to extend a session.
     // Use it to limit write operations. Set to 0 to always update the database.
     // Note: This option is ignored if using JSON Web Tokens
@@ -84,8 +83,7 @@ export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/options#jwt
   jwt: {
     // A secret to use for key generation (you should set this explicitly)
-    secret: 'testsecret',
-    maxAge: 60 * 60 * 24 * 30,
+    // secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw',
     // Set to true to use encryption (default: false)
     // encryption: true,
     // You can define your own encode/decode functions for signing and encryption
@@ -100,7 +98,7 @@ export const authOptions: NextAuthOptions = {
   // pages is not specified for that route.
   // https://next-auth.js.org/configuration/pages
   pages: {
-    signIn: '/auth/signin',  // Displays signin buttons
+    //signIn: '/signin',  // Displays signin buttons
     // signOut: '/auth/signout', // Displays form with sign out button
     // error: '/auth/error', // Error code passed in query string as ?error=
     // verifyRequest: '/auth/verify-request', // Used for check email page
@@ -119,15 +117,13 @@ export const authOptions: NextAuthOptions = {
       else if (new URL(url).origin === baseUrl) return url;
       return baseUrl;
     },
-    async session(session, token, user) {
+    async session(session, user) {
       if (session.user) {
         session.user.id = user.id;
       }
       return session;
     },
-    async jwt(token, user, account, profile, isNewUser) { 
-      return token 
-    }
+    // async jwt(token, user, account, profile, isNewUser) { return token }
   },
 
   // Events are useful for logging
